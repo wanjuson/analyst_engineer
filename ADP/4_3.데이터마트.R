584f2f2f27ed7bdb56d7688a64967e90ad0bd9f7
wanjuson <92307467+wanjuson@users.noreply.github.com>
2022-06-12 14:46:17 +0900

    Update 4_3.데이터마트.R

--- ADP/4_3.데이터마트.R
+++ ADP/4_3.데이터마트.R
@@ -4,8 +4,11 @@ nchar("test")
 length("test")
 
 # Q26
-t <- c(1,2,3,NA)
-mean(t)
-mean(t, na.rm = T)
+t1 <- c(1,2,3,NA)
+mean(t1)
+mean(t1, na.rm = T)
 
+t2 <- c(1,2,3, '')
+mean(t2)
+mean(t2, na.rm = T)
 
